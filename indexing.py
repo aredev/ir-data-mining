b5306ad37cb477b7ff9f31aa0a54746f48646703
aredev <appierasool@gmail.com>
2017-09-20 14:23:32 +0200

    Indexing begin

---
+++ indexing.py
@@ -0,0 +1,44 @@
+from whoosh.fields import Schema, TEXT, ID
+from whoosh.index import create_in, open_dir
+import os.path
+from whoosh.query import *
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.qparser import QueryParser
+
+
+
+# Create fake data
+
+schema = Schema(
+    docId=ID(stored=True),
+    content=TEXT,
+   )
+
+# Create inde
+
+if not os.path.exists("index"):
+    os.mkdir("index")
+ix = create_in("index", schema)
+
+ix = open_dir("index")
+
+writer = ix.writer()
+writer.add_document(docId=u"8", content=u"This is my document")
+writer.add_document(docId=u"9", content=u"This is my plop")
+writer.add_document(docId=u"10", content=u"Life like this")
+writer.add_document(docId=u"11", content=u"document document document")
+
+writer.commit()
+
+# Find te
+
+
+with ix.searcher() as searcher:
+    parser = QueryParser("content", ix.schema)
+    myquery = parser.parse(u"This document")
+    print(myquery)
+    results = searcher.search(myquery)
+    print(len(results))
+    if len(results) > 0:
+        for r in results:
+            print(r)
