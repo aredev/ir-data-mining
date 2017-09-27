ef9ab48e8703290e9c360de2f55045ca24e64c3c
aredev <appierasool@gmail.com>
2017-09-27 15:24:10 +0200

    Start with lda

--- lda.py
+++ lda.py
@@ -1,4 +1,15 @@
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+
 import gensim
 
+
 class LDA(object):
-    
+    corpus = gensim.corpora.MalletCorpus('nips.mallet')
+
+    #Build a model
+    model = gensim.models.LdaModel(corpus, id2word=corpus.id2word, alpha='auto', num_topics=25)
+    model.save('nips.lda')
+
+    model = gensim.models.LdaModel.load('nips.lda')
+    print(model.show_topics())
