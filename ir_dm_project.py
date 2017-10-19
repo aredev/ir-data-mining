a0a32338318bfbb3dec7d2c9b65972c812c23c4d
matthias-ghering <matthias_ghering@live.nl>
2017-10-19 22:45:28 +0200

    small changes

--- ir_dm_project.py
+++ ir_dm_project.py
@@ -5,7 +5,7 @@ def main():
     # index = Indexer()
     author_clusters = AuthorClustering(cache_enabled=True)
     #print(author_clusters.find_authors_by_paper("3206"))
-    print("suggestions: " + str(author_clusters.find_authors_by_paper("3719"))) #paper 3719 auth 3758
+    print("suggestions:" + author_clusters.find_authors_by_paper("3719")) #paper 3719 auth 3758
 
 
 if __name__ == "__main__":
