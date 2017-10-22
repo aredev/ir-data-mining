693ee80442fa6d117a94bcb8c2520e043178561f
matthias-ghering <matthias_ghering@live.nl>
2017-10-22 22:31:24 +0200

    some clean up

--- graph_cluster.py
+++ graph_cluster.py
@@ -13,14 +13,13 @@ class GraphCluster:
     def shortest_path_dict(self):
         return nx.all_pairs_dijkstra_path_length(self.graph)
 
-    # Colors nodes based on the labels. The order of the labels must match the order of the nodes
     def plot_graph(self, filename, labels="black", outlier_label=-1):
         if not outlier_label == -1 and not labels == "black":
             labels = [outlier_label if x == -1 else x for x in labels]
         plt.figure(num=None, figsize=(10, 10), dpi=1200)
         pos = nx.spring_layout(self.graph)
-        nx.draw_networkx_nodes(self.graph, pos, width=15, node_color=labels, node_size=2, alpha=1)
-        nx.draw_networkx_edges(self.graph, pos, width=1, edge_color='r', alpha=1)
+        nx.draw_networkx_nodes(self.graph, pos, node_color=labels, node_size=2, alpha=1)
+        nx.draw_networkx_edges(self.graph, pos, width=0.5, edge_color='r', alpha=1)
 
         plt.savefig(filename)
         plt.clf()
@@ -38,46 +37,9 @@ class GraphCluster:
                 j += 1
             i += 1
         if len(path_dict.keys()) < 100:
-            print(np.matrix(matrix))
+            #print(np.matrix(matrix))
         return np.matrix(matrix)
 
-    # LEGACY CODE: this could offer another solution for clustering disconnected graphs (DBSCAN)
-    # The matrix conversion does not handle unconnected graphs.
-    def path_dict_to_sparse_matrix(self, path_dict):
-        print(path_dict)
-        print("items: " + str(path_dict.items()))
-        print("values: " + str(path_dict.values()))
-        row_ind = []
-        col_ind = []
-        data = []
-
-        A = nx.adjacency_matrix(self.graph)
-        print(A.todense())
-
-        for key1, v in path_dict.items():
-            print("v: " + str(v))
-            print("key1: " + str(key1))
-
-        """
-        row_ind.append(key1)
-        col_ind.append(key2)
-        data.append(val)
-        x = sp.csr_matrix(([1] * len(row_ind), (row_ind, col_ind)))
-        """
-
-        #row_ind2 = [k for k, v in path_dict.items() for _ in range(len(v))]
-        #col_ind2 = [i for ids in path_dict.values() for i in ids]
-        #print("row_ind: " + str(row_ind))
-        #print("col_ind: " + str(col_ind))
-        #x = sp.csr_matrix(([1] * len(row_ind2), (row_ind2, col_ind2)))
-        #print(x)
-        #return x
-
-    # DBSCAN with sparce matrix problem with missing indexes (e.g 0)
-    def cluster_dbscan2(self):
-        path_dict = nx.all_pairs_shortest_path_length(self.graph)
-        db = skcluster.DBSCAN(eps=1, metric="precomputed", algorithm="ball trees").fit(self.path_dict_to_sparse_matrix(path_dict))
-        return db.labels_
 
     # dbscan clustering using computing all complete subgraphs separately.
     def cluster_dbscan(self, min_node_threshold=0, epsilon=1.0, sample_min=5):
@@ -116,7 +78,6 @@ class GraphCluster:
         label_list = [x for _, x in sorted(zip(index_list, label_list), key=lambda x: int(x[0]))]
         return label_list
 
-    # FIX: dissconnected graph and  k estimation
     def cluster_agglomerative(self, min_node_threshold=1):
         graph_list = nx.connected_component_subgraphs(self.graph)
         nr_of_graphs = nx.number_connected_components(self.graph)
